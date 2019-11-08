86985424fb5c15a90d2de6ee0174211bcae15c23
tee29909 <59160571@go.buu.ac.th>
2019-11-08 23:26:45 +0700

    first commit

---
+++ app/src/main/java/buu/informatics/s59160102/luckyperson/random.kt
@@ -0,0 +1,24 @@
+package buu.informatics.s59160102.luckyperson
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class random : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_random, container, false)
+    }
+
+
+}
