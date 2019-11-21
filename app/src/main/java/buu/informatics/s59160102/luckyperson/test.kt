d8e536aa593b94fdbab12e5865258e7f9b8d60c1
tee29909 <59160571@go.buu.ac.th>
2019-11-21 15:45:59 +0700

    test

--- app/src/main/java/buu/informatics/s59160102/luckyperson/test.kt
+++ app/src/main/java/buu/informatics/s59160102/luckyperson/test.kt
@@ -2,9 +2,11 @@ package buu.informatics.s59160102.luckyperson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import buu.informatics.s59160102.luckyperson.databinding.ActivityTestBinding
@@ -20,6 +22,8 @@ class test : Fragment() {
         val binding: ActivityTestBinding = DataBindingUtil.inflate(
             inflater, R.layout.activity_test, container, false)
 
+ binding.button.setOnClickListener {  Toast.makeText(context, "hello", Toast.LENGTH_LONG).show()
+     Log.i("AddFragment", "ได้มะ") }
 
 
         binding.setLifecycleOwner(this)
